7329df997bb8e1e5d8602b2ef87a3e6d7ef54fad
Eriberto Lima <eribertosflima@gmail.com>
2023-05-10 19:51:46 -0300

    refactor: deixa o regex de forma estatica para alocar menos memoria na class Cpf

--- src/entities/Cpf.ts
+++ src/entities/Cpf.ts
@@ -1,8 +1,10 @@
 class Cpf {
   private _valor: string;
+  private static _regex =
+  /^(([0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2})|([0-9]{11}))$/;
 
   constructor(valor: string) {
-    if (!/^(([0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2})|([0-9]{11}))$/.test(valor)) { 
+    if (!Cpf._regex.test(valor)) { 
       throw new Error('CPF iválido');
     }
     this._valor = valor;
