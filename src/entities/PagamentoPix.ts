65f9e6151343c3fe019a9541ac72e382166ba6b4
Eriberto Lima <eribertosflima@gmail.com>
2023-05-10 18:26:13 -0300

    feat: add pagamento pix

---
+++ src/entities/PagamentoPix.ts
@@ -0,0 +1,25 @@
+import IPagamentoEfetivavel from '../interfaces/IpagamentoEfetivavel';
+import Conta from './Conta';
+import Pagamento from './Pagamento';
+
+class PagamentoPix extends Pagamento implements IPagamentoEfetivavel {
+  private _chave: string;
+
+  constructor(
+    chave: string,
+    valor:number,    
+    origem: Conta,
+    destino: Conta,
+  ) {
+    super(valor, origem, destino);
+    this._chave = chave;
+  }
+
+  public efetivar(): string {
+    this._origem.debitar(this._valor);
+    this._destino.creditar(this._valor);
+    return this.gerarCodigo();
+  }
+}
+
+export default PagamentoPix;
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import Conta from './entities/Conta';
 // import Pagamento from './entities/Pagamento';
 import PagamentoBoleto from './entities/PagamentoBoleto';
 import PagamentoCartao from './entities/PagamentoCartao';
+import PagamentoPix from './entities/PagamentoPix';
 import IPagamentoEfetivavel from './interfaces/IpagamentoEfetivavel';
 
 const contaEriberto = new Conta(1000, '837.530.823-41');
@@ -10,6 +11,8 @@ const contaEriberto = new Conta(1000, '837.530.823-41');
 const contaAgiota = new Conta(50000, '657.326.803-22');
 
 const contaSorveteria = new Conta(3000, '837.530.823-51');
+
+const contaBanco = new Conta(800000000000, '397.326.803-22');
 
 const pagamentoAgiota = new PagamentoBoleto(
   30,
@@ -24,6 +27,13 @@ const pagamentoSorvete = new PagamentoCartao(
   contaSorveteria,
 );
 
+const pagamentoCarro = new PagamentoPix(
+  '11959037783',
+  1500,
+  contaEriberto,
+  contaBanco,
+);
+
 const efetivarPagamentos = (pagamentos: IPagamentoEfetivavel[]): void => {     
   for (const pagamento of pagamentos) {
     const codigo = pagamento.efetivar();
@@ -31,4 +41,4 @@ const efetivarPagamentos = (pagamentos: IPagamentoEfetivavel[]): void => {
   }
 };
 
-efetivarPagamentos([pagamentoAgiota, pagamentoSorvete]);
+efetivarPagamentos([pagamentoAgiota, pagamentoSorvete, pagamentoCarro]);
